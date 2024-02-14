b6c41ad1a22b73b2f27fdc72782db2dec14464ff
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-02-14 17:08:46 -0300

    Actualizo Status Code de los métodos

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
@@ -10,33 +11,36 @@ def user_api_view(request):
     if request.method == 'GET':
         users = User.objects.all()
         users_serializer = UserSerializer(users, many=True)
-        return Response(users_serializer.data)
+        return Response(users_serializer.data, status=status.HTTP_200_OK)
     
     elif request.method == 'POST':
         user_serializer = UserSerializer(data=request.data)
         if user_serializer.is_valid():
             user_serializer.save()
-            return Response(user_serializer.data)
-        return Response(user_serializer.errors)
+            return Response({"message":"User created successfully!"}, status=status.HTTP_201_CREATED)
+                             
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 @api_view(['GET', 'PUT', 'DELETE'])
 def user_detail_api_view(request, pk=None):
+
     user = User.objects.filter(id=pk).first()
+
     if user:
         if request.method == 'GET':            
             user_serializer = UserSerializer(user)
-            return Response(user_serializer.data)
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
         
-        if request.method == 'PUT':            
+        elif request.method == 'PUT':            
             user_serializer = UserSerializer(user, data=request.data)
             if user_serializer.is_valid():
                 user_serializer.save()
-                return Response(user_serializer.data)
-            return Response(user_serializer.errors)
+                return Response(user_serializer.data, status=status.HTTP_200_OK)
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-        if request.method == 'DELETE':
+        elif request.method == 'DELETE':
             username = user.username
             user.delete()
-            return Response({'message': f'User {username} deleted'}, status=200)
+            return Response({'message': f'User {username} deleted'}, status=status.HTTP_200_OK)
         
-    return Response({'message': 'User not found'}, status=400)
+    return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
