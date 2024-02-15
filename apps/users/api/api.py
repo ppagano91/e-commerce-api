2d6df629286155579c759bbb635a9b922c985c42
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-02-15 17:07:17 -0300

    Encriptación de Password

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from apps.users.models import User
-from apps.users.api.serializers import UserSerializer
+from apps.users.api.serializers import UserSerializer, UserListSerializer
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -9,8 +9,8 @@ from rest_framework.decorators import api_view
 @api_view(['GET', 'POST'])
 def user_api_view(request):
     if request.method == 'GET':
-        users = User.objects.all()
-        users_serializer = UserSerializer(users, many=True)
+        users = User.objects.all().values('id','username','email','password')
+        users_serializer = UserListSerializer(users, many=True)
         return Response(users_serializer.data, status=status.HTTP_200_OK)
     
     elif request.method == 'POST':
--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -5,4 +5,33 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         # fields = ('id','username','email','name','last_name','image','is_active','is_staff')
-        fields = '__all__'    #Other option: '__all__'
+        fields = '__all__'
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+    
+    def update(self, instance, validated_data):
+        print(instance)
+        print(validated_data)
+        updated_user = super().update(instance, validated_data)
+        updated_user.set_password(validated_data['password'])
+        updated_user.save()
+
+        return updated_user
+
+
+class UserListSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+
+    def to_representation(self, instance):
+        return {
+            'id': instance["id"],
+            'username': instance["username"],
+            'email': instance["email"],
+            'password': instance["password"],
+        }
