a2f11b0c8402280643cf265145aff715c0cc3860
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-02-22 11:06:39 -0300

    Agrego RetrieveAPIView y DestroyAPIView

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from apps.products.api.views.general_views import MeasureUnitListAPIView, CategoryListAPIView, IndicatorListAPIView
-from apps.products.api.views.products_views import ProductListAPIView, ProductCreateAPIView
+from apps.products.api.views.products_views import ProductListAPIView, ProductCreateAPIView, ProductRetrieveAPIView, ProductDestroyAPIView
 
 urlpatterns = [
     path('measure_unit/', MeasureUnitListAPIView.as_view(), name='measure-unit-list'),
@@ -8,5 +8,8 @@ urlpatterns = [
     path('indicator/', IndicatorListAPIView.as_view(), name='indicator-list'),
     path('product/list/', ProductListAPIView.as_view(), name='product-list'),
     path('product/create/', ProductCreateAPIView.as_view(), name='product-create'),
+    path('product/retrieve/<int:pk>/', ProductRetrieveAPIView.as_view(), name='product-retrieve'),
+    path('product/destroy/<int:pk>/', ProductDestroyAPIView.as_view(), name='product-destroy'),
+
 
 ]
