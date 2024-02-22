deb12f724cb9db8519a1c17a13f808ffbdc3c4c1
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-02-22 17:33:13 -0300

    Agrego ViewSets y Routers

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from apps.products.api.views.general_views import MeasureUnitListAPIView, CategoryListAPIView, IndicatorListAPIView
-from apps.products.api.views.products_views import ProductListCreateAPIView, ProductRetrieveUpdateDestroyApiView
 
 urlpatterns = [
     path('measure_unit/', MeasureUnitListAPIView.as_view(), name='measure-unit-list'),
     path('category_product/', CategoryListAPIView.as_view(), name='category-product-list'),
-    path('indicator/', IndicatorListAPIView.as_view(), name='indicator-list'),    
-    path('product/', ProductListCreateAPIView.as_view(), name='product-list-create'),
-    path('product/retrieve/<int:pk>/', ProductRetrieveUpdateDestroyApiView.as_view(), name='product-retrieve-update-destroy'),
-]
+    path('indicator/', IndicatorListAPIView.as_view(), name='indicator-list')
+    ]
---
+++ apps/products/api/views/products_viewsets.py
@@ -0,0 +1,44 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework import viewsets
+
+from apps.base.api import GeneralListApiView
+from apps.products.api.serializers.product_serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    
+    def get_queryset(self, pk=None):
+        if pk == None:
+            return self.get_serializer().Meta.model.objects.filter(state=True)
+        return self.get_serializer().Meta.model.objects.filter(id=pk, state=True).first()
+    
+    def list(self, request):
+        product_queryset = self.get_queryset()
+        product_serializer = self.get_serializer(product_queryset, many=True)
+        return Response(product_serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"Producto agregado correctamente"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        product = self.get_queryset(pk)
+        if product:
+            product_serializer = self.serializer_class(product, data=request.data)
+            if product_serializer.is_valid():
+                product_serializer.save()
+                return Response(product_serializer.data, status=status.HTTP_200_OK)
+            return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":f"No existe un producto con id {pk}"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        product = self.get_queryset(pk)
+        if product:
+            product.state = False
+            product.save()
+            return Response({"message":"Producto eliminado correctamente"}, status=status.HTTP_200_OK)
+        return Response({"message":f"No existe un producto con id {pk}"}, status=status.HTTP_400_BAD_REQUEST)
