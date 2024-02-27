9853abf29e27487ba9492df84802192a7c0fcd8b
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-02-27 15:17:20 -0300

    Agrego autenticación por Token para listar productos

---
+++ apps/users/authentication.py
@@ -0,0 +1,38 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import exceptions
+from datetime import timedelta
+from django.utils import timezone
+from django.conf import settings
+
+
+class ExpiringTokenAuthentication(TokenAuthentication):    
+
+    def expires_in(self, token):
+        time_elapsed = timezone.now() - token.created
+        left_time = timedelta(seconds=settings.TOKEN_EXPIRED_AFTER_SECONDS) - time_elapsed
+        return left_time
+    
+    def is_token_expired(self, token):
+        return self.expires_in(token) < timedelta(seconds=0)
+    
+    def token_expire_handler(self, token):
+        is_expired = self.is_token_expired(token)
+        if is_expired:
+            user = token.user
+            token.delete()
+            token = self.get_model().objects.create(user=user)
+
+        return token
+    
+    def authenticate_credentials(self, key):
+        model = self.get_model()
+        user = None
+        try:
+            token = model.objects.select_related("user").get(key=key)
+            user = token.user
+            token = self.token_expire_handler(token)
+            
+        except model.DoesNotExist:
+            message="Invalid token"
+
+        return user
--- apps/users/views.py
+++ apps/users/views.py
@@ -8,7 +8,23 @@ from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 
+from apps.users.authentication_mixins import Authentication
 from apps.users.api.serializers import UserTokenSerializer
+
+class UserToken(Authentication, APIView):
+    def get(self, request):        
+        try:
+            username = UserTokenSerializer.Meta.model.objects.filter(username=self.user.username).first()
+            user_token, _ = Token.objects.get_or_create(user = username)
+
+            if user_token:
+                user = user_token.user
+                token = user_token.key
+                user_serializer = UserTokenSerializer(user)
+                return Response({"token": token, "user":user_serializer}, status=status.HTTP_200_OK)
+            return Response({"error": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({"error": "Token not found in the request"}, status=status.HTTP_404_NOT_FOUND)
 
 class Login(ObtainAuthToken):
     
@@ -19,13 +35,11 @@ class Login(ObtainAuthToken):
 
             if user.is_active:
                 token, created = Token.objects.get_or_create(user=user)
-                print("user\n", user)
                 user_serializer = UserTokenSerializer(user)
 
                 if created:
                     return Response({'token': token.key, 'user': user_serializer.data, 'message': 'Token created successfuly!'}, status=status.HTTP_201_CREATED)
                 else:
-                    '''
                     all_sessions = Session.objects.filter(expire_date__gte=datetime.now())
                     
                     if all_sessions.exists():
@@ -34,17 +48,10 @@ class Login(ObtainAuthToken):
                                 session_data = session.get_decoded()
                                 if user.id == int(session_data.get('_auth_user_id')):
                                     session.delete()
-                    #    else:
-                    #       session_data = all_sessions.first().get_decoded()
-                    #       if user.id == int(session_data.get('_auth_user_id')):
-                    #           all_sessions.first().delete()
-                    
                     token.delete()
                     token = Token.objects.create(user=user)
                     return Response({'token': token.key, 'user': user_serializer.data, 'message': 'Token created successfuly!'}, status=status.HTTP_201_CREATED)
-                    '''
-                    token.delete()
-                    return Response({"error": "This user already has an associated token"}, status=status.HTTP_409_CONFLICT)                
+                    # return Response({"error": "This user already has an associated token"}, status=status.HTTP_409_CONFLICT)                
             else:
                 return Response({'error': 'This user is not active'}, status=status.HTTP_401_UNAUTHORIZED)
             
